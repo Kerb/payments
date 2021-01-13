de84082adbf4c249d447e99fced720896d26fc12
Andrey Klyuchko <Klyuchko.An@sberbank.ru>
2021-01-13 19:44:00 +0300

    Initial commit.

---
+++ web-app/src/main/java/net/kerba/payments/app/PaymentsApp.java
@@ -0,0 +1,20 @@
+package net.kerba.payments.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Входная точка для старта приложения.
+ */
+@SpringBootApplication
+@SuppressWarnings({"HideUtilityClassConstructor"})
+public class PaymentsApp {
+
+    /**
+     * Метод запускает приложение.
+     * @param args аргументы
+     */
+    public static void main(final String[] args) {
+        SpringApplication.run(PaymentsApp.class, args);
+    }
+}
